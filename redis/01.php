6112c8c12aed5be11298507d675f1b78a6e4f014
fanrong33 <fanrong33@qq.com>
2018-04-14 23:09:17 +0800

    update

--- redis/01.php
+++ redis/01.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * php中Redis函数的用法总结
- * 
- * http://www.php.cn/php-weizijiaocheng-380858.html
+ * [php中Redis函数的用法总结](http://www.php.cn/php-weizijiaocheng-380858.html)
  */
 
 $redis = new Redis(); 
-$redis->connect('127.0.0.1', 6379) or die("Could not connect redis"); 
+$result = $redis->connect('127.0.0.1', 6379); // or die("Could not connect redis");
+var_dump($result); // 1
+
 $effect = $redis->auth('123456');  // 登录验证密码，返回 true、false
-var_dump($effect);
+var_dump($effect); // true
 
 $redis->set('key','123');
 var_dump($redis->get('key'));
@@ -16,8 +16,10 @@ var_dump($redis->get('key'));
 $redis->delete('key');
 var_dump($redis->get('key'));
 
-$redis->setex('key2', 10, 'value');
+// $redis->setex('key2', 10, 'value');
 var_dump($redis->get('key2'));
 
 
 $redis->close();//释放资源
+
+
